a66dfb499648e5c9dbf7a1ac8e8a07bae9c2a125
oz-om <ozyoom@gmail.com>
2023-01-27 10:26:34 +0000

    eruda

--- src/global/Eruda.js
+++ src/global/Eruda.js
@@ -1,13 +1,15 @@
 //console
 import eruda from "eruda";
-
-eruda.init({
-  tool: ["console", "elements", "snippets", "network", "resources", "sources"],
-  defaults: {
-    displaySize: 50,
-    transparency: 0.9,
-    theme: "Monokai Pro",
-  },
-});
+const mode = process.env.VITE_MODE;
+if (mode == "development") {
+  eruda.init({
+    tool: ["console", "elements", "snippets", "network", "resources", "sources"],
+    defaults: {
+      displaySize: 50,
+      transparency: 0.9,
+      theme: "Monokai Pro",
+    },
+  });
+}
 
 // eruda.show();
